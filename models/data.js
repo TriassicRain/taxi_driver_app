383c61b5ac769a6f4e29cb564cc6524e4ab8437b
Celeste Glavin <cmugla@gmail.com>
2016-08-04 15:19:00 -0400

    front end talks to python data

--- models/data.js
+++ models/data.js
@@ -1,27 +1,26 @@
 const request           = require('request');
-const ENIGMA_KEY        =process.env.ENIGMA_KEY
-
 
 module.exports = {
   dataReturn (req,res,next){
     console.log('Made it to model')
-    const url1 = 'https://localhost:4000/predict'
+    const url1 = 'http://localhost:4000/predict'
 
     let lat = req.query.lat;
     let lng = req.query.lng;
-    let time = req.query.time;
+    let time = parseInt(req.query.time);
 
     request.get({
       url: url1,
       qs: {
         'pickup_lat': lat,
-        'pickup_long': lng,
+        'pickup_lon': lng,
         'hour': time
       }
     }, function(err,response,body){
       if(err) throw err;
-      let price = JSON.parse(body);
-      res.results = price.result;
+      console.log(body)
+      let price = JSON.parse(body)
+      res.results = price
       next()
     })
   }
