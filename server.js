60b0a5e37700805af0db7ca16e50d879ab61cdc8
John Sigmon <johnasigmon@gmail.com>
2016-08-04 13:10:33 -0400

    migrated everything to organization

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+const express         = require('express');
+const path            = require('path');
+const logger          = require('morgan');
+const bodyParser      = require('body-parser');
+const methodOverride  = require('method-override');
+const homeRoute       = require('./controllers/homes');
+const resultsRoute       = require('./controllers/results');
+
+const app             = express();
+const port            = process.env.PORT || 3000;
+
+
+app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/', homeRoute);
+app.use('/results', resultsRoute);
+app.get('/dummydata', function(req,res){
+  res.send('{"price": "3.50"}')
+})
+
+app.listen(port, function() {
+  console.log('Server is listening on ',port);
+})
